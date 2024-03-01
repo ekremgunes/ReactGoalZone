2f78412d50801092239302591e842377cdfcf4d6
ekremgunes <github1357@gmail.com>
2024-03-01 15:43:25 +0300

    some fixes

--- src/components/LastGame.jsx
+++ src/components/LastGame.jsx
@@ -13,21 +13,35 @@ const initialGameData = {
   homeScoreFH: 0,
   awayScore: 0,
   awayScoreFH: 0,
-  duration: null,
+  minute: 0,
+  duration: ''
+};
+
+const getMatchMin = (date) => {
+  const matchDate = new Date(date)
+  const now = new Date()
+  const diffInMs = now - matchDate
+  const diffInMin = Math.floor(diffInMs / 60000)
+  return diffInMin + 1
 };
 
 const LastGame = () => {
-  const id = useSelector((state) => state.user.id);
-  const [lastGame, setLastGame] = useState(initialGameData);
+  const id = useSelector((state) => state.user.id)
+  const [lastGame, setLastGame] = useState(initialGameData)
+  const [loading, setLoading] = useState(true)
 
-  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/api/teams/${id}/matches?status=IN_PLAY,FINISHED&limit=1`);
+        const response = await fetch(`/api/teams/${id}/matches?status=IN_PLAY,PAUSED,FINISHED&limit=1`)
         const data = await response.json()
         console.log(data)
+        if (!data.matches) {
+          setLoading(true)
+          return
+        }
 
         setLastGame({
           status: data.matches[0].status,
@@ -39,31 +53,32 @@ const LastGame = () => {
           homeScoreFH: data.matches[0].score.halfTime.home,
           awayScore: data.matches[0].score.fullTime.away,
           awayScoreFH: data.matches[0].score.halfTime.away,
-          duration:data.matches[0].score.duration,
+          duration: data.matches[0].score.duration,
+          minute: getMatchMin(data.matches[0].utcDate)
         });
-        setLoading(false);
-
+        setLoading(false)
       } catch (error) {
-        console.error('Veri getirme hatası:', error);
-        setLoading(true);
+        console.error('Veri getirme hatası:', error)
+        setLoading(true)
       }
     };
 
     fetchData();
 
-    if (lastGame.status == "IN_PLAY") {
-      var fetchInterval = setInterval(() => {
-        fetchData();
-      }, 10000);
+    // if (lastGame.status == "IN_PLAY") {
+    //   var fetchInterval = setInterval(() => {
+    //     fetchData();
+    //   }, 20000);
 
-      return () => { clearInterval(fetchInterval) }
+    //   return () => { clearInterval(fetchInterval) }
 
-    }
+    // }
+  }, [id])
 
-  }, [id]);
+
 
   if (loading) {
-    return <></>;
+    return <></>
   }
 
   return (
@@ -76,11 +91,11 @@ const LastGame = () => {
 
           <div className="d-flex team-vs">
             <span className="score">{lastGame.homeScore} - {lastGame.awayScore}</span>
-            <span className="score" style={{ top: "9%", fontSize: "13px", letterSpacing: "5px" }}>
-              {lastGame.status != "IN_PLAY" ? lastGame.status : `${lastGame.duration}'`}
+            <span className="score" style={{ top: "8%", fontSize: "13px", letterSpacing: "2px" }}>
+              {lastGame.status != "IN_PLAY" ? lastGame.status : `${lastGame.minute}'`}
             </span>
             <span className="score" style={{ top: "16%", fontSize: "9px" }}>FH({lastGame.homeScoreFH} - {lastGame.awayScoreFH})</span>
-            
+
 
             <div className="team-1 w-50">
               <div className="team-details w-100 text-center">
--- src/components/NextGame.jsx
+++ src/components/NextGame.jsx
@@ -4,107 +4,129 @@ import { useSelector, useDispatch } from "react-redux";
 import "../helpers/helpers.js";
 
 const initialGameData = {
+  homeTeamId: 0,
   homeTeamImage: '',
   awayTeamImage: '',
   homeTeam: '',
   awayTeam: '',
   competition: '',
-  date:''
-};
+  date: '',
+  matchWeek: 0,
+  venue: ''
+}
 
+const formatDate = (utcDate) => {
+  const options = {
+    weekday: 'long',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+
+  return new Date(utcDate).toLocaleString('en-US', options);
+}
 const NextGame = () => {
-  const id = useSelector((state) => state.user.id);
-  const [nextGame, setNextGame] = useState(initialGameData);
+  const id = useSelector((state) => state.user.id)
+  const [nextGame, setNextGame] = useState(initialGameData)
+  const [loading, setLoading] = useState(true)
 
-  const [loading, setLoading] = useState(true);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`/api/teams/${id}/matches?status=SCHEDULED&limit=1`)
+      const data = await response.json()
+      if (!data.matches) {
+        setLoading(true)
+        return
+      }
+      setNextGame((prev)=>({
+        ...prev,
+        date: data.matches[0].utcDate,
+        homeTeamId: data.matches[0].homeTeam.id,
+        homeTeamImage: data.matches[0].homeTeam.crest,
+        awayTeamImage: data.matches[0].awayTeam.crest,
+        homeTeam: data.matches[0].homeTeam.shortName,
+        awayTeam: data.matches[0].awayTeam.shortName,
+        competition: data.matches[0].competition.name,
+        matchWeek: data.matches[0].matchday,
+      }));
+      setLoading(false)
+
+    } catch (error) {
+      console.error('Veri getirme hatası:', error)
+      setLoading(true)
+    }
+  };
+
+  const fetchHomeTeam = async () => {
+    try {
+
+      if (nextGame.homeTeamId > 0) {
+        const responseOfTeam = await fetch(`/api/teams/${nextGame.homeTeamId}`)
+        const dataOfTeam = await responseOfTeam.json()
+
+        if (dataOfTeam) {
+          setNextGame((prev) => ({
+            ...prev,
+            venue: dataOfTeam.venue
+          }));
+        }
+
+      }
+
+    } catch (error) {
+      console.error('Veri getirme hatası:', error)
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`/api/teams/${id}/matches?status=SCHEDULED&limit=1`);
-        const data = await response.json()
-        console.log(data)
+    fetchData()    
+  }, [id])
 
-        setNextGame({
-          date: data.matches[0].utcDate,
-          homeTeamImage: data.matches[0].homeTeam.crest,
-          awayTeamImage: data.matches[0].awayTeam.crest,
-          homeTeam: data.matches[0].homeTeam.shortName,
-          awayTeam: data.matches[0].awayTeam.shortName,          
-          competition:data.matches[0].competition.name,
-        });
-        setLoading(false);
-
-      } catch (error) {
-        console.error('Veri getirme hatası:', error);
-        setLoading(true);
-      }
-    };
-
-    fetchData();
-
-    if (nextGame.status == "IN_PLAY") {
-      var fetchInterval = setInterval(() => {
-        fetchData();
-      }, 10000);
-
-      return () => { clearInterval(fetchInterval) }
-
-    }
-
-  }, [id]);
+  useEffect(() => {
+    fetchHomeTeam()
+  }, [nextGame.homeTeamId])
 
   if (loading) {
-    return <></>;
+    return <></>
   }
 
-  const formatDate = (utcDate) => {
-    const options = {
-      weekday: 'long',
-      month: 'numeric',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric'
-    };
-  
-    return new Date(utcDate).toLocaleString('en-US', options);
-  };
-  
+
 
   return (
-<div className="widget-next-match">
-                <div className="widget-title">
-                  <h3>Next Match</h3>
-                </div>
-                <div className="widget-body mb-3">
-                  <div className="widget-vs">
-                    <div className="d-flex align-items-center justify-content-around justify-content-between w-100">
-                      <div className="team-1 text-center">
-                        <img src={nextGame.homeTeamImage} alt="Image" />
-                        <h3>{nextGame.homeTeam}</h3>
-                      </div>
-                      <div>
-                        <span className="vs"><span>VS</span></span>
-                      </div>
-                      <div className="team-2 text-center">
-                        <img src={nextGame.awayTeamImage} alt="Image" />
-                        <h3>{nextGame.awayTeam}</h3>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+    <div className="widget-next-match">
+      <div className="widget-title">
+        <h3>Match Week {nextGame.matchWeek} </h3>
+      </div>
+      <div className="widget-body mb-3">
+        <div className="widget-vs">
+          <div className="d-flex align-items-center justify-content-around justify-content-between w-100">
+            <div className="team-1 text-center">
+              <img src={nextGame.homeTeamImage} alt="Image" />
+              <h3>{nextGame.homeTeam}</h3>
+            </div>
+            <div>
+              <span className="vs"><span>VS</span></span>
+            </div>
+            <div className="team-2 text-center">
+              <img src={nextGame.awayTeamImage} alt="Image" />
+              <h3>{nextGame.awayTeam}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
 
-                <div className="text-center widget-vs-contents mb-4">
-                  <h4>{nextGame.competition}</h4>
-                  <p className="mb-5">
-                  <span className="d-block">{formatDate(nextGame.date)}</span>
-                    <strong className="text-primary">New Euro Arena</strong>
-                  </p>
+      <div className="text-center widget-vs-contents mb-4">
+        <h4>{nextGame.competition}</h4>
+        <p className="mb-5">
+          <span className="d-block">{formatDate(nextGame.date)}</span>
+          <strong className="text-primary">{nextGame.venue}</strong>
+        </p>
 
-                  <div id="date-countdown2" className="pb-1"></div>
-                </div>
-              </div>
-   
+        <div id="date-countdown2" className="pb-1"></div>
+      </div>
+    </div>
+
 
   )
 }
